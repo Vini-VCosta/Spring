aed8abedd13c7b504b646d589b1c3ebabc69bc8c
Vini-VCosta <vini.vcosta11@gmail.com>
2020-06-16 00:10:02 -0300

    ExemploH2Database

---
+++ exemploH2Database/src/main/java/costavinicius/exemploH2Database/ExemploH2DatabaseApplication.java
@@ -0,0 +1,13 @@
+package costavinicius.exemploH2Database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploH2DatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploH2DatabaseApplication.class, args);
+	}
+
+}
---
+++ exemploH2Database/src/test/java/costavinicius/exemploH2Database/ExemploH2DatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package costavinicius.exemploH2Database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploH2DatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
